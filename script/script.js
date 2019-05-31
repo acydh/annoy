1c95e51dec55ea230e1b733d3775c2caab66c960
Acydh <spadavecchia.andrea@spes.uniud.it>
2019-05-31 18:15:40 +0100

    add annoy button v1

---
+++ script/script.js
@@ -0,0 +1,35 @@
+// //////////////////////////////////////
+// ANNOYING BUTTON V1
+// //////////////////////////////////////
+
+var followingCounter = 0;
+var annoyingButton = document.querySelector(".annoying-button");
+
+annoyingButton.addEventListener('mouseenter', startFollowing);
+
+function startFollowing() {
+  if (followingCounter <= 2) {
+    annoyingButton.removeEventListener('mouseenter', startFollowing);
+    window.addEventListener('mousemove', moveButton);
+    setTimeout(stopFollowing, 1000);
+    followingCounter++;
+  } else {
+    vanish();
+  }
+}
+
+function stopFollowing() {
+  window.removeEventListener('mousemove', moveButton);
+  annoyingButton.addEventListener('mouseenter', startFollowing);
+}
+
+function moveButton() {
+  annoyingButton.style.top = event.clientY + 20 + "px";
+	annoyingButton.style.left = event.clientX + 80 + "px";
+}
+
+function vanish(){
+  annoyingButton.style.animationName = "buttonVanish";
+  annoyingButton.style.animationDuration = "1s";
+  setTimeout(function(){annoyingButton.style.display = "none";}, 900);
+}
