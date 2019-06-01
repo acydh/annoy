3c5c06299bc011f7021bd001f4071d78e5f1cc61
Acydh <spadavecchia.andrea@spes.uniud.it>
2019-06-01 12:38:13 +0100

    add annoying cookies

--- script/script.js
+++ script/script.js
@@ -24,18 +24,53 @@ function stopFollowing() {
 }
 
 function moveButton() {
-  annoyingButton.style.top = event.clientY + 20 + "px";
-	annoyingButton.style.left = event.clientX + 80 + "px";
+  annoyingButton.style.top = event.clientY - 40 + "px";
+	annoyingButton.style.left = event.clientX - 70 + "px";
 }
 
 function vanish(){
   annoyingButton.style.animationName = "buttonVanish";
   annoyingButton.style.animationDuration = "1s";
-  setTimeout(function(){annoyingButton.style.display = "none";}, 900);
+  setTimeout(() => {
+    annoyingButton.style.display = "none";
+  }, 900);
+  setAnnoyingCursor();
 }
+
+
+// //////////////////////////////////////
+// ANNOYING CURSOR V1
+// //////////////////////////////////////
+
+
 
 function setAnnoyingCursor() {
   document.documentElement.style.cursor = "wait";
 }
 
-setAnnoyingCursor();
+
+
+// //////////////////////////////////////
+// ANNOYING COOKIES V1
+// //////////////////////////////////////
+
+
+const cookieBox = document.querySelector(".annoy-cookies");
+const closeCookies = document.querySelector(".close");
+closeCookies.addEventListener("click", hideCookies);
+var showTimes = 0;
+
+function showAnnoyingCookies() {
+  cookieBox.style.bottom = "0";
+}
+
+function hideCookies() {
+  cookieBox.style.bottom = "-200px";
+  if (showTimes < 1) {
+    setTimeout(showAnnoyingCookies, 2000);
+    showTimes++;
+  }
+
+}
+
+showAnnoyingCookies()
